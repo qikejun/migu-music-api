e8c9f4b982486c294b0982e348f298bb7f7b624c
maoyunpeng <maoyp@avic.com>
2021-07-06 08:34:36 +0800

    新增专辑

---
+++ api/album.js
@@ -0,0 +1,7 @@
+/*
+ * @Descripttion:
+ * @Author: pengpeng
+ * @Date: 2021-07-06 08:34:23
+ * @LastEditors: pengpeng
+ * @LastEditTime: 2021-07-06 08:34:24
+ */
---
+++ routers/album.js
@@ -0,0 +1,7 @@
+/*
+ * @Descripttion:
+ * @Author: pengpeng
+ * @Date: 2021-07-06 08:30:44
+ * @LastEditors: pengpeng
+ * @LastEditTime: 2021-07-06 08:30:45
+ */
