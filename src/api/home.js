c8baf485fc2319f6113b51ed3dc69e27f916b647
maoyunpeng <maoyp@avic.com>
2021-07-07 08:55:31 +0800

    更新首页接口

--- src/api/home.js
+++ src/api/home.js
@@ -1,43 +1,45 @@
 /*
- * @Descripttion: 
+ * @Descripttion:
  * @Author: pengpeng
  * @Date: 2021-07-05 08:49:37
- * @LastEditors: pengpeng
- * @LastEditTime: 2021-07-05 08:56:20
+ * @LastEditors: peng
+ * @LastEditTime: 2021-07-07 08:48:57
  */
-/**
- * @msg: 推荐歌单
- * @param {
- * pageNo: 默认 1
- * type: 1: 推荐，2: 最新，默认推荐
- * }
- * @return {*}
- */
-export const recommendPlaylist = {
-  url: '/recommend/playlist',
-  method: 'GET'
-}
-/**
- * @msg: 新歌
- * @param {
- * pageNo: 默认 1
- * pageSize:  默认 10
- * }
- * @return {*}
- */
-export const newSongs = {
-  url: '/new/songs',
-  method: 'GET'
-}
-/**
- * @msg: 新专辑
- * @param {
- * pageNo: 默认 1，
- * pageSize: 默认 10
- * }
- * @return {*}
- */
-export const newAlbums = {
-  url: '/new/albums',
-  method: 'GET'
-}
+module.exports = {
+  /**
+   * @msg: 推荐歌单
+   * @param {
+   * pageNo: 默认 1
+   * type: 1: 推荐，2: 最新，默认推荐
+   * }
+   * @return {*}
+   */
+  recommendPlaylist: {
+    url: "/recommend/playlist",
+    method: "GET",
+  },
+  /**
+   * @msg: 新歌
+   * @param {
+   * pageNo: 默认 1
+   * pageSize:  默认 10
+   * }
+   * @return {*}
+   */
+  newSongs: {
+    url: "/recommend/playlist",
+    method: "GET",
+  },
+  /**
+   * @msg: 新专辑
+   * @param {
+   * pageNo: 默认 1，
+   * pageSize: 默认 10
+   * }
+   * @return {*}
+   */
+  newAlbums: {
+    url: "/new/albums",
+    method: "GET",
+  },
+};
--- src/routers/home.js
+++ src/routers/home.js
@@ -2,10 +2,26 @@
  * @Descripttion:
  * @Author: pengpeng
  * @Date: 2021-07-05 08:56:47
- * @LastEditors: pengpeng
- * @LastEditTime: 2021-07-06 08:43:25
+ * @LastEditors: peng
+ * @LastEditTime: 2021-07-07 08:55:12
  */
-const Router = require('@koa/router')
-const api = require('../apiManage/index')
+const Router = require("@koa/router");
+const api = require("../apiManage/index");
 
-const router = new Router()
+const homeRouter = new Router();
+
+homeRouter.get("/recommendPlaylist", async (ctx, next) => {
+  const params = {
+    keyword: "张学友",
+    pgc: pageNum,
+    rows: pageSize,
+    type: typeMap["song"],
+  };
+  await api.request("home.recommendPlaylist", params).then((res) => {
+    console.log(123, res);
+    ctx.status = 200;
+    ctx.body = JSON.stringify(res);
+  });
+});
+
+module.exports = homeRouter;
